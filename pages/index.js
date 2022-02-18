42d0f4f7f8c8db156b2eec48e8abd27041eaf39e
Angi <wqminc@gmail.com>
2022-02-17 19:58:07 -0600

    added more infor on home page

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,9 @@ const index =() => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
     <h1>Hi. I'm Angi. Welcome to my blog.</h1>
-    <p>I am learning how to deploy a Next.js app to AWS. I don't know what the heck I'm doing. Fun times. I started building this app 1 week ago. The past 2 days have been frustrating, because I can figure out how I commit changes to Github in VSC. Grrrrrr!!</p>
+    <p>I am learning how to deploy a Next.js app to AWS. I don't know what the heck I'm doing. Fun times. I started building this app 1 week ago. The past 2 days have been frustrating, because I can figure out how I commit changes to Github in VSC. Grrrrrr!!  Not sure what is happening. It is the 17th of February 2022 on Friday eve and I sit here yelling at my computer for not pushing my code. What am I doing wrong. Of course none of the YouTube videos cover any of these issues that you may encounter ... </p><br>
+
+    
     <Layout />
     <h2>
         <Link href="/posts/first-post">
