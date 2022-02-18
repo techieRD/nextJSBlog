221f8760ad3de63a2047f29bc6ae87271cfc8b0a
Angi <wqminc@gmail.com>
2022-02-17 19:53:07 -0600

    Added more blurb to Home page

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ const index =() => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
     <h1>Hi. I'm Angi. Welcome to my blog.</h1>
-    <p>I am learning how to deploy a Next.js app to AWS. I don't know what the heck I'm doing. Fun times.</p>
+    <p>I am learning how to deploy a Next.js app to AWS. I don't know what the heck I'm doing. Fun times. I started building this app 1 week ago. The past 2 days have been frustrating, because I can figure out how I commit changes to Github in VSC. Grrrrrr!!</p>
     <Layout />
     <h2>
         <Link href="/posts/first-post">
