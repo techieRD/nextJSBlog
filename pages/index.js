25c234922f464dc0f1c2cca97d4fb08cd95132a5
Angi <wqminc@gmail.com>
2022-02-17 19:47:49 -0600

    add more blurb to home page

--- pages/index.js
+++ pages/index.js
@@ -8,10 +8,11 @@ const index =() => {
   return (
     <div>
       <Head>
-        <title>Angie's Blog</title>
+        <title>Angie's NextJS Blog</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
     <h1>Hi. I'm Angi. Welcome to my blog.</h1>
+    <p>I am learning how to deploy a Next.js app to AWS. I don't know what the heck I'm doing. Fun times.</p>
     <Layout />
     <h2>
         <Link href="/posts/first-post">
